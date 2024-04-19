26fae2bb17a60edaddf81271aeab48cfb9f8fcc8
Piwko Patryk <patryk.piwko@god.dev>
2024-04-19 23:47:44 +0200

    first commit

---
+++ src/test/java/com/seniority/shelter/ShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniority.shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
