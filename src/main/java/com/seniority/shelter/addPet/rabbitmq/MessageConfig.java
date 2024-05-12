7a492ef07bfae23d031c087f4677cc9e23ab8085
Piwko Patryk <patryk.piwko@god.dev>
2024-05-12 22:04:14 +0200

    set message converter

--- src/main/java/com/seniority/shelter/addPet/rabbitmq/MessageConfig.java
+++ src/main/java/com/seniority/shelter/addPet/rabbitmq/MessageConfig.java
@@ -4,10 +4,13 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 
 @Configuration
 @EnableAutoConfiguration
@@ -38,5 +41,10 @@ public class MessageConfig {
                 .bind(petQueue())
                 .to(appExchange())
                 .with(petRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
