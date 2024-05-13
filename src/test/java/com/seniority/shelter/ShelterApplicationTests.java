ddcae0e94ee163a6b01cb6198303e7d0f3d0309c
Piwko Patryk <patryk.piwko@god.dev>
2024-05-13 19:31:25 +0200

    use rabbit from docker

--- src/test/java/com/seniority/shelter/ShelterApplicationTests.java
+++ src/test/java/com/seniority/shelter/ShelterApplicationTests.java
@@ -1,13 +1,9 @@
 package com.seniority.shelter;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShelterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
