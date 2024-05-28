0e870eaebb81f2802d7e95c0bda4ad1b4e8e1f9e
Piwko Patryk <patryk.piwko@god.dev>
2024-05-28 11:55:41 +0200

    change endpoint to add pet to shelter

--- src/main/java/com/seniority/shelter/addPet/AddPetRestService.java
+++ src/main/java/com/seniority/shelter/addPet/AddPetRestService.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/pet")
+@RequestMapping("/api/place/add-pet")
 @RequiredArgsConstructor
 public class AddPetRestService {
 
